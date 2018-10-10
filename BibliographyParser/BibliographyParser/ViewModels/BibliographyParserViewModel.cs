7153a27265002693c440a6b394970e4509fb8bd9
Adrian Standowicz <adrian.standowicz@gmail.com>
2018-10-10 21:30:43 +0200

    Package Reference, Fody, MVVM demo
    Migrated BibliographyPaser and XSLSerializer to package reference. Added Fody and PropertyChanged.Fody packages. Added a simple MVVM flow demo.

---
+++ BibliographyParser/BibliographyParser/ViewModels/BibliographyParserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using PropertyChanged;
+using System.Threading;
+
+namespace BibliographyParser.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class BibliographyParserViewModel
+    {
+        public string TestString { get; set; } = string.Empty;
+
+        public BibliographyParserViewModel()
+        {
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                Thread.Sleep(5000);
+                TestString = "Hello bindings!";
+            }).Start();
+        }
+    }
+}
---
+++ BibliographyParser/BibliographyParser/Views/BibliographyParser.xaml.cs
@@ -0,0 +1,18 @@
+﻿using BibliographyParser.ViewModels;
+using System.Windows;
+
+namespace BibliographyParser.Views
+{
+    /// <summary>
+    /// Interaction logic for BibliographyParser.xaml
+    /// </summary>
+    public partial class BibliographyParser : Window
+    {
+        public BibliographyParser()
+        {
+            var viewModel = new BibliographyParserViewModel();
+            this.DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
